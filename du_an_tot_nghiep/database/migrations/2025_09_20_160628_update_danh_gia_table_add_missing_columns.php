b28f2336351ce2d1cc18c8170361b5336cb09a17
truongan2605 <anltph51720@gmail.com>
2025-09-21 11:50:20 +0700

    done db version 1

--- du_an_tot_nghiep/database/migrations/2025_09_20_160628_update_danh_gia_table_add_missing_columns.php
+++ du_an_tot_nghiep/database/migrations/2025_09_20_160628_update_danh_gia_table_add_missing_columns.php
@@ -6,28 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('danh_gia', function (Blueprint $table) {
-            $table->dropColumn(['diem_so', 'is_public']);
-            $table->smallInteger('diem')->unsigned()->after('nguoi_dung_id');
-            $table->json('anh')->nullable()->after('noi_dung');
-            $table->enum('trang_thai_kiem_duyet', ['cho_kiem_duyet', 'da_dang', 'bi_tu_choi'])->default('cho_kiem_duyet')->after('anh');
+            $table->dropColumn(['anh', 'trang_thai_kiem_duyet']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('danh_gia', function (Blueprint $table) {
-            $table->dropColumn(['diem', 'anh', 'trang_thai_kiem_duyet']);
-            $table->integer('diem_so')->unsigned();
-            $table->boolean('is_public')->default(true);
+            $table->json('anh')->nullable();
+            $table->enum('trang_thai_kiem_duyet', ['cho_kiem_duyet', 'da_dang', 'bi_tu_choi'])->default('cho_kiem_duyet');
         });
     }
 };
