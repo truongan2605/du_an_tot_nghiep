9294780c0cea5a1cd89b459ca86f656bd019be05
truongan2605 <anltph51720@gmail.com>
2025-09-27 11:33:13 +0700

    fix router

--- du_an_tot_nghiep/resources/views/partials/admin-sidebar.blade.php
+++
@@ -1,9 +0,0 @@
-<aside class="admin-sidebar">
-    <ul>
-        {{-- <li><a href="{{ route('admin.rooms.index') }}">Quản lý phòng</a></li> --}}
-        <li><a href="#">Quản lý phòng</a></li>
-        <li><a href="#">Quản lý khách hàng</a></li>
-        <li><a href="#">Đặt phòng</a></li>
-        <li><a href="#">Dịch vụ</a></li>
-    </ul>
-</aside>
