b7552d0d9d2a9bf13f1f5ddc0c47d71df30cea72
truongan2605 <anltph51720@gmail.com>
2025-10-03 14:59:52 +0700

    fix image detail room client

--- du_an_tot_nghiep/app/Http/Controllers/Client/RoomController.php
+++ du_an_tot_nghiep/app/Http/Controllers/Client/RoomController.php
@@ -1,21 +1,22 @@
 <?php
-
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Phong;
 
 class RoomController extends Controller
 {
     public function show($id)
     {
-        // $phong = Phong::with(['loaiPhong', 'tang', 'images', 'tienNghis'])->findOrFail($id);
-        // return view('detail-room', compact('phong'));
-        // Lấy phòng theo ID
-        $phong = Phong::findOrFail($id);
-        
-        // Trả về view và truyền dữ liệu phòng
-        return view('detail-room', compact('phong'));
+        $phong = Phong::with(['loaiPhong','tang','images','tienNghis'])->findOrFail($id);
+
+        $related = Phong::with('images')
+            ->where('loai_phong_id', $phong->loai_phong_id)
+            ->where('id', '<>', $phong->id)
+            ->orderByDesc('created_at') 
+            ->take(5)
+            ->get();
+
+        return view('detail-room', compact('phong', 'related'));
     }
 }
