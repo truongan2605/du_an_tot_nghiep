a8d9d252b9b168d6c1ffa4023d054c791716253f
atus23 <149553920+atus23@users.noreply.github.com>
2025-11-13 21:50:24 +0700

    save user_voucher

--- du_an_tot_nghiep/database/migrations/2025_11_06_130045_create_user_voucher_table.php
+++ du_an_tot_nghiep/database/migrations/2025_11_06_130045_create_user_voucher_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('user_voucher', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('voucher_id')->constrained('voucher')->onDelete('cascade');
+            $table->timestamp('claimed_at')->nullable(); // thời gian user nhận mã
             $table->timestamps();
+
+            $table->unique(['user_id', 'voucher_id']); // mỗi user chỉ được nhận 1 lần
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('user_voucher');
